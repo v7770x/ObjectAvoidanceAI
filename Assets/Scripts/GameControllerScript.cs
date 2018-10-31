74df24c4de039b03bfcb2f58ce292522fec548ef
Vishwa Sheth <vishwa@v1020-wn-113-6.campus-dynamic.uwaterloo.ca>
2018-10-31 17:08:11 -0400

    new commits

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour {
+
+    public static float scrollSpeed = 10;
+
+    //public static float getScrollSpeed() {
+    //    return scrollSpeed;
+    //}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour {
+
+    private float scrollSpeed = GameControllerScript.scrollSpeed;
+    Rigidbody2D rgbd;
+
+	// Use this for initialization
+	void Start () {
+        rgbd = GetComponent<Rigidbody2D>();
+        rgbd.velocity = new Vector2(0, -1 * scrollSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
